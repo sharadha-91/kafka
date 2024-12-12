3f0fa08d83bc7ba22bd280f476462ca462bbcf2a
sharadha-91 <76049782+sharadha-91@users.noreply.github.com>
2024-12-12 10:31:28 +0530

    Add files via upload
    consumer

---
+++ Kafka-Consumer/src/main/java/com/consumer/service/KafkaMessageListener.java
@@ -0,0 +1,37 @@
+package com.consumer.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import com.consumer.dto.Customer;
+
+@Service
+public class KafkaMessageListener {
+
+	 Logger log = LoggerFactory.getLogger(KafkaMessageListener.class);
+
+	    @KafkaListener(topics = "javatechie-demo2",groupId = "jt-group")
+	    public void consumeEvents(Customer customer) {
+	        log.info("consumer consume the events {} ", customer.toString());
+	    }
+
+	    //for different partition we can create different consumer instance
+	    
+//	    @KafkaListener(topics = "javatechie-demo1",groupId = "jt-group-new")
+//	    public void consume2(String message) {
+//	        log.info("consumer2 consume the message {} ", message);
+//	    }
+	//
+//	    @KafkaListener(topics = "javatechie-demo1",groupId = "jt-group-new")
+//	    public void consume3(String message) {
+//	        log.info("consumer3 consume the message {} ", message);
+//	    }
+	//
+//	    @KafkaListener(topics = "javatechie-demo1",groupId = "jt-group-new")
+//	    public void consume4(String message) {
+//	        log.info("consumer4 consume the message {} ", message);
+//	    }
+	
+}
